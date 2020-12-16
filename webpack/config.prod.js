473a4f33c18067f16c8a5f9d540d618566769b9f
Stephen <appcypher@outlook.com>
2020-12-16 14:55:39 +0100

    Fix webpack es module issue

--- webpack/config.prod.js
+++ webpack/config.prod.js
@@ -5,47 +5,51 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin');
 module.exports = {
   mode: 'production',
   devtool: 'source-map',
-  entry: './src/index.js',
+  entry: './src/main.js',
   output: {
     path: path.join(__dirname, '../dist'), // directory where bundle is dropped
     filename: 'bundle.min.js',
   },
   module: {
     rules: [
-    {
-      test: /\.vue$/,
-      loader: 'vue-loader'
-    },
-    {
-      test: /\.html$/,
-      use: ['html-loader'],
-    },
-    {
-      test: /\.scss$/,
-      use: ['vue-style-loader', 'style-loader', 'css-loader', 'sass-loader'],
-    },
-    {
-      test: /\.(png|jpg|gif)$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {},
-        },
-      ],
-    },
-    {
-      test: /\.(ttf|eot|woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-      use: [
-        {
-          loader: 'url-loader',
-          options: {},
-        },
-      ],
-    },
-    {
-      test: /\.svg$/,
-      loader: 'svg-inline-loader'
-    }
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['vue-style-loader', 'style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(ttf|eot|woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              esModule: false
+            },
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-url-loader'
+      }
     ],
   },
   plugins: [
